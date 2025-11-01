63ed3fe062991b229bf8693f798d8ce5c5f2b017
Faizan <mohammed.faizan@adaptiveaiventures.com>
2025-11-02 01:39:02 +0530

    deleted background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.webRequest.onCompleted.addListener(
-  (details) => {
-    if (details.url.includes("/voyager/api/jobs/jobPostings/")) {
-      chrome.tabs.sendMessage(details.tabId, { url: details.url });
-    }
-  },
-  { urls: ["https://www.linkedin.com/voyager/api/jobs/jobPostings/*"] }
-);
